fcfa790faaa009c4a60c874b5a3919f804710889
gokultej <38723397+gokultej@users.noreply.github.com>
2023-08-07 16:38:43 +0530

    ADD URL endpoint for yearly and all data

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pandas as pd
 from flask import Flask,render_template
 import pandas
 app = Flask(__name__)
-
+stations=pd.read_csv("data_small/stations.txt" , skiprows=17)
+stations=stations[["STAID","STANAME                                 "]]
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",data=stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
@@ -15,5 +16,21 @@ def about(station,date):
     return {"station":station,
             "date": date,
             "temperature":temperature}
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename="D:\Weather_Data_API_Run\data_small\TG_STAID"+str(station).zfill(6)+".txt"
+    df=pd.read_csv(filename,skiprows=20,parse_dates=['    DATE'])
+    result=df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    filename="D:\Weather_Data_API_Run\data_small\TG_STAID"+str(station).zfill(6)+".txt"
+    df=pd.read_csv(filename,skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result=df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
 if __name__=="__main__":
     app.run(debug=True)
